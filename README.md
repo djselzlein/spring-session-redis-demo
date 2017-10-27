05950e7ae88631f16841c357911ec31f244bf13f
Djeison Selzlein <djeisonaguir@gmail.com>
2017-10-27 13:43:11 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-session-redis-demo
---
+++ src/main/java/com/selzlein/djeison/springsessionredisdemo/SpringSessionRedisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.selzlein.djeison.springsessionredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionRedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionRedisDemoApplication.class, args);
+	}
+}
